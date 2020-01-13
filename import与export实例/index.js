1ea861f7f3d1f83731322e112400294e661e1952
417143864@qq.com <417143864@qq.com>
2020-01-13 10:53:05 +0800

    imort与export实例

--- import与export实例/index.js
+++ import与export实例/index.js
@@ -5,7 +5,9 @@ let ele={
     loginInput:document.querySelector("#loginInput"),
     mainDiv:document.querySelector("#mainDiv"),
     userName:document.querySelector("#loginName"),
-    longinPanel:document.querySelector("#longinPanel")
+    longinPanel:document.querySelector("#longinPanel"),
+    heroList:document.querySelector("#hexoList"),
+    skillList:document.querySelector("#skillList")
 }
 let game = new Game();
 window.onload=function(){
@@ -19,7 +21,25 @@ window.onload=function(){
 
             game.login(userName);
             game.loadHeroes();
-            console.log(game)
+           console.log(game)
+            game.player.heroes.forEach(hero => {
+            let li = document.createElement("li");
+            li.innerHTML = `英雄名称：${hero.name}<img src='./sources/heros/${hero.ico}' />`;
+            li.onclick = function () {
+                ele.skillList.innerHTML = "";
+                hero.skills.forEach((skill, index) => {
+                    let skillLi = document.createElement("li");
+                    skillLi.onclick = function () {
+                        // console.log(11);
+                        // hero.fire(index);
+                       // hero.fire.before(addFn,hero,index);
+                    }
+                    skillLi.innerHTML = `技能名称：${skill.name}<img src='./sources/skills/${skill.ico}' />`;
+                    ele.skillList.appendChild(skillLi);
+                })
+            }
+            ele.heroList.appendChild(li);
+        })
         }
     }
 }
