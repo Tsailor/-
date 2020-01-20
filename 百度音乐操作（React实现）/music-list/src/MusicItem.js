0db5541b2120adedd2c45035d870da686428d97d
Tsailor <417143864@qq.com>
2020-01-20 18:41:04 +0800

    音乐列表操作-react实现

--- 百度音乐操作（React实现）/music-list/src/MusicItem.js
+++ 百度音乐操作（React实现）/music-list/src/MusicItem.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
-
+import "./index.css"
 class MusicItem extends Component {
     constructor(){
         super();
+
+        this.handleChoose=this.handleChoose.bind(this);
+        this.handleRemove=this.handleRemove.bind(this);
+        this.handleCollect=this.handleCollect.bind(this);
+    }
+    handleChoose(){
+       const { id,choose }=this.props;
+       choose(id);
+    }
+    handleCollect(e){
+        e.preventDefault();
+        const {id, collect}=this.props;
+        collect(id);
+    }
+    handleRemove(e){
+        e.preventDefault();
+        const { id , removeit }=this.props;
+        removeit(id)
     }
     render(){
-        const { content }=this.props;
+        const {content}=this.props;
         return(
-            <div>
-                <p>{content.title}</p>
-            </div>
+            <li className="MusicItem">
+                <input type="checkbox" className="checkbox"
+                       checked={content.checked} onChange={this.handleChoose}/>
+                <span>{content.title}</span>
+                {content.collect ? <a onClick={this.handleCollect} href="">取消收藏</a> 
+                    : <a onClick={this.handleCollect} href="">收藏</a>}
+                <a href="" onClick={this.handleRemove}>删除</a>
+            </li>
         )
     }
 }
--- 百度音乐操作（React实现）/music-list/src/index.js
+++ 百度音乐操作（React实现）/music-list/src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MusicItem from './MusicItem.js';
-
+import "./index.css";
 class MusicList extends Component{
     constructor(){
         super();
@@ -33,16 +33,118 @@ class MusicList extends Component{
                 checked: true,
                 collect: false //是否收藏 true 收藏 false 没有收藏
                 }
-            ]
+            ],
+            isCheckeddAll:"",
+            inputValue:""
         }
+        this.chooseAll=this.chooseAll.bind(this);    //   全选
+        this.handleChange=this.handleChange.bind(this);   // 输入框输入
+        this.choose=this.choose.bind(this);      //   选择单个
+        this.removeit=this.removeit.bind(this);   //  删除单个
+        this.collect=this.collect.bind(this);     //  收藏
+        this.handleRemove=this.handleRemove.bind(this);  // 处理批量 删除
+        this.handleAdd=this.handleAdd.bind(this);    //  处理添加
+    }
+    chooseAll(e){
+        let {data,isCheckeddAll}=this.state;
+     //  console.log(e.target)
+       if(e.target.name==="chooseAll"){
+           if(e.target.checked){
+                data.map((v) => (
+                    v.checked=true
+                ));
+               isCheckeddAll=true;
+           }else{
+               data.map((v) => (
+                   v.checked = false
+               ));
+               isCheckeddAll=false;
+           }
+           this.setState({data,isCheckeddAll});
+       }
+    }
 
+    // 选择单个
+    choose(id){
+        let { data,isCheckeddAll}=this.state;
+        data.map((v)=>
+            v.id===id ? v.checked=(!v.checked) : null
+        )
+        isCheckeddAll=data.every((v) =>(
+            v.checked===true
+        ))
+        this.setState({data,isCheckeddAll})
+    }
+
+    removeit(id){
+        let _data=this.state.data;
+        _data.map((v,k)=>(
+            v.id===id ? _data.splice(k,1) : null
+        ))
+        this.setState({
+            data:_data
+        })
+    }
+
+    // 输入
+    handleChange(e){
+        if(e.target.name==="input"){
+            let input=e.target.value;
+            this.setState({inputValue:input});
+           // console.log(this.state);
+        }
+    }
+    //  处理添加
+    handleAdd(e){
+        e.preventDefault();
+      //  console.log(this.state.inputValue);
+        if(this.state.inputValue!==""){
+            let newItem = {
+                id: this.state.data[this.state.data.length - 1].id + 1,
+                title: this.state.inputValue,
+                checked: false,
+                collect: false
+            }
+            let data = this.state.data;
+            data.push(newItem);
+            this.setState({ data: data,inputValue:"" });
+        }
+        
+    }
+    // 处理收藏
+    collect(id){
+        let data=this.state.data;
+        console.log(data)
+        data.map((v)=>
+           v.id===id ? v.collect=(!v.collect) :null
+        )
+        this.setState({data:data});
+    }
+
+    // 处理批量删除
+    handleRemove(e){
+        e.preventDefault(); //  重要！！！
+        let _data=this.state.data.filter((v)=> 
+            v.checked!==true
+        )
+        this.setState({data:_data});
     }
     render(){
         return(
-           <div>
-               {this.state.data.map((item) =>(
-                   <MusicItem key={item.id} content={item}/>
-                ))}
+           <div className="container">
+                <h1 className="title">百度音乐榜单</h1>
+               {this.state.data.map((item) =>
+                   <MusicItem key={item.id} content={item} choose={this.choose} 
+                       collect={this.collect} id={item.id} removeit={this.removeit}/>
+                )}
+                <div className="footer">
+                    <input type="checkbox" name="chooseAll" className="checkbox"  
+                           checked={this.state.isCheckeddAll} onChange={this.chooseAll}/>
+                    <span>全选/全不选</span>
+                    <a href="" className="remove" onClick={this.handleRemove}>删除</a>
+                    <input type="text" name="input" value={this.state.inputValue} onChange={this.handleChange}></input>
+                    <a href="" className="add" onClick={this.handleAdd}>添加</a>
+                </div>
            </div>
         )
     }
