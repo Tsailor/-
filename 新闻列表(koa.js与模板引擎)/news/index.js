4883207742fae23a87bdc980354362fcb355978a
Tsailor <417143864@qq.com>
2020-02-17 18:50:19 +0800

    xiuzheng

--- 新闻列表(koa.js与模板引擎)/news/index.js
+++
@@ -1,8 +0,0 @@
-const Koa = require("koa");
-const Router = require("koa-router");
-const Views = require("koa-views");
-const Static = require("koa-static");
-const koaNunjucks = require('koa-nunjucks-2');
-const path = require('path');
-
-const app = new Koa();
