1290eb99d4a09068cde7376f342bea9e266ac31a
Tsailor <417143864@qq.com>
2020-07-02 22:55:42 +0800

    add barrage(Node.js、mysql)

--- 视频弹幕(SSE、node.js)/barrage/server.js
+++ 视频弹幕(SSE、node.js)/barrage/server.js
@@ -2,7 +2,8 @@ const http = require("http");
 const { URL } = require("url");   //新版本url接口，旧版已弃
 const path = require("path");
 const fs =require("fs");
-const mysql = require("mysql")
+const mysql = require("mysql");
+const sse = require("sse")
 const mime = require("./data/mime.json");
 
 let  base = 'http://localhost:4001/';
@@ -37,7 +38,7 @@ let server = http.createServer((req,res)=>{
                 postD += v
             });
             req.on("end", async () => {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.setHeader('Content-Type', 'application/json' );
                 let postData = JSON.parse(postD).message;   //写入数据库的数据
                 console.log(postData);
                 connection.query("INSERT INTO barrage(message) VALUES(?) ",[postData],(error, results, fields)=>{
@@ -48,12 +49,24 @@ let server = http.createServer((req,res)=>{
                 });
             })
             req.on("error",()=> {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.setHeader('Content-Type', 'application/json' );
                 let result= { status:0,message:"error!"}
                 res.end(JSON.stringify(result))
             })
         }
-    }else{
+    }else if(pathName === '/sse'){   //  sse服务器推送数据
+        res.writeHead(200, { "Content-Type":"text/event-stream",                           
+                            "Cache-Control":"no-cache",  
+                            "Connection":"keep-alive"});
+        let  interval = setInterval(function () { 
+            connection.query("SELECT * FROM barrage ORDER BY id DESC LIMIT 0,5"),(error, results, fields)=>{
+                if(error) throw error;
+                console.log(results) 
+            }, 6*1000); 
+        req.connection.addListener("close", function () { 
+            clearInterval(interval); 
+        }, false);
+    } else {
         let extName = path.extname(req.url);
         res.setHeader("Content-type",mime[extName]);
         let extData = fs.readFileSync("./static"+ req.url);
