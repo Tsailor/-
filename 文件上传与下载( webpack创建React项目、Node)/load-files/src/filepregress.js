00d7cb7f341204696b32af432be8eb1da4390479
Tsailor <417143864@qq.com>
2020-07-10 23:03:07 +0800

    antd

---
+++ 文件上传与下载( webpack创建React项目、Node)/load-files/src/filepregress.js
@@ -0,0 +1,13 @@
+import React , { Component } from 'react';
+import style from './index.css';
+class FilePregress  extends Component {
+
+    render(){
+        return(
+            <div> 进度</div>
+         
+        )
+    }
+}
+export default FilePregress;
+
